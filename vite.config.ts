0eb24116e974badd286900c1a92cd52ba89521b6
Peter Simon <1401202+loonkwil@users.noreply.github.com>
2023-09-28 22:22:30 +0200

    Add action to create a GitHub page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/beat-the-ai/",
   plugins: [react()],
   resolve: {
     alias: { "~": fileURLToPath(new URL("./src", import.meta.url)) },
